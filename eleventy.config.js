a21967190618e36d7871cef6f151b014987decbc
fzakfeld <freerk@zakfeld.com>
2024-04-13 22:48:06 +0200

    New Website

---
+++ eleventy.config.js
@@ -0,0 +1,24 @@
+const tailwind = require("tailwindcss");
+const tailwindConfig = require("./tailwind.config");
+const postCss = require("postcss");
+const autoprefixer = require("autoprefixer");
+
+const postcssFilter = (css, done) => {
+  postCss([tailwind(tailwindConfig), autoprefixer()])
+    .process(css)
+    .then(
+      (res) => done(null, res.css),
+      (err) => done(err, null)
+    );
+};
+
+module.exports = function (config) {
+  config.addNunjucksAsyncFilter("postcss", postcssFilter);
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
